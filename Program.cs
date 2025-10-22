ec03fdcc565f694f3c933903c2f1cb738da9841f
Натали <n@gmail.com>
2025-10-22 11:18:20 +0300

    Добавлено вычисление площади (S = πR²)

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace App1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус: ");
+            double R = double.Parse(Console.ReadLine());
+
+            double C = 2 * Math.PI * R;
+            double S = Math.PI * Math.Pow(R, 2);
+
+            Console.WriteLine($"Окружность: {C}");
+            Console.WriteLine($"Площадь: {S}");
+        }
+    }
+}
