6e193091481a1cc56b818b645a0ab2a47ba71477
Натали <n@gmail.com>
2025-10-22 11:26:21 +0300

    Добавлено вычисление окружности (C = 2πR)

--- Program.cs
+++ Program.cs
@@ -10,12 +10,8 @@ namespace App1
             double R = double.Parse(Console.ReadLine());
 
             double C = 2 * Math.PI * R;
-            double S = Math.PI * Math.Pow(R, 2);
-            double V = (4.0 / 3.0) * Math.PI * Math.Pow(R, 3);
 
             Console.WriteLine($"Окружность: {C}");
-            Console.WriteLine($"Площадь: {S}");
-            Console.WriteLine($"Объем: {V}");
         }
     }
 }
